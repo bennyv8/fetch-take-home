1920b2fb16c2cc01017e740dbe7fc72715227470
bennyv8 <bvanhr@gmail.com>
2023-12-20 16:54:24 -0800

    implemented match modal and match functionality

--- src/components/modals/MatchModal.tsx
+++ src/components/modals/MatchModal.tsx
@@ -1,8 +1,24 @@
-const MatchModal = () => {
+interface MatchModal {
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+const MatchModal = ({ isOpen, onClose, children }: MatchModal) => {
   return (
-    <div>
-      <h1>Congratulations you've matched with...</h1>
-    </div>
+    isOpen && (
+      <div className="fixed left-0 top-0 flex h-full w-full items-center justify-center bg-black bg-opacity-50">
+        <div className="relative w-96 rounded bg-white p-8 shadow-lg">
+          <button
+            className="absolute right-2 top-2 rounded bg-blue-500 px-2 py-1 text-xs text-white"
+            onClick={onClose}
+          >
+            X
+          </button>
+          <div className="pointer-events-none">{children}</div>
+        </div>
+      </div>
+    )
   );
 };
 
--- src/pages/favorites/FavoritesPage.tsx
+++ src/pages/favorites/FavoritesPage.tsx
@@ -16,9 +16,30 @@ interface DogProps {
 const FavoritesPage = () => {
   const { favoritesList } = useContext(FavoritesContext);
   const [favoritedDogs, setFavoritedDogs] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [matchedDog, setMatchedDog] = useState<DogProps[]>([]);
 
-  const handleMatchClick = () => {
-    fetchEndpoint("/dogs/match", "POST", JSON.stringify([...favoritesList]));
+  const handleMatchClick = async () => {
+    try {
+      const { match } = await fetchEndpoint(
+        "/dogs/match",
+        "POST",
+        JSON.stringify([...favoritesList]),
+      );
+      const dogMatch = await fetchEndpoint(
+        "/dogs/",
+        "POST",
+        JSON.stringify([match]),
+      );
+      setMatchedDog(dogMatch);
+      setIsModalOpen(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
   };
 
   useEffect(() => {
@@ -37,8 +58,10 @@ const FavoritesPage = () => {
       {favoritedDogs.map((dog: DogProps, index) => {
         return <DogCard {...dog} key={index} />;
       })}
-      <button>Find Match!</button>
-      <MatchModal />
+      <button onClick={handleMatchClick}>Find Match!</button>
+      <MatchModal isOpen={isModalOpen} onClose={handleModalClose}>
+        <DogCard {...matchedDog[0]} />
+      </MatchModal>
     </div>
   );
 };
